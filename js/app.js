92bacf9130fd56e05a6990f69b72131bc254e1aa
Bruno Grande <contact@brunogrande.me>
2015-05-10 21:28:52 -0700

    implemented clear completed button

--- js/app.js
+++ js/app.js
@@ -15,10 +15,29 @@
 		};
 
 		$scope.completeAll = function(){
-			console.log($scope.isAllCompleted);
 			angular.forEach($scope.todoList, function(task){
 				task.completed = $scope.isAllCompleted;
 			});
+		};
+
+		$scope.clearCompleted = function() {
+			var oldList = $scope.todoList;
+			$scope.todoList = [];
+			angular.forEach(oldList, function(task) {
+				if (!task.completed) {
+					$scope.todoList.push(task);
+				}
+			});
+		};
+
+		$scope.getNumCompleted = function() {
+			var counter = 0;
+			angular.forEach($scope.todoList, function(task) {
+				if (task.completed) {
+					counter++;
+				}
+			});
+			return counter;
 		};
 	});
 
