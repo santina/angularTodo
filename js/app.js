3ac01d37dab04adb2c434c113550a0651e6164e2
Bruno Grande <contact@brunogrande.me>
2015-05-10 23:16:59 -0700

    implemented doubleclick to edit and change todoList to object

--- js/app.js
+++ js/app.js
@@ -4,28 +4,36 @@
 	var app = angular.module('myTodoApp', []);
 
 	app.controller('todoCtrl', function($scope) {
-		$scope.todoList = [];
+		$scope.idCounter = 1;
+		$scope.todoList = {};
 		$scope.isAllCompleted = false;
+
+		$scope.createId = function() {
+			var oldIdCounter = $scope.idCounter;
+			$scope.idCounter = oldIdCounter + 1;
+			return oldIdCounter;
+		};
 
 		$scope.addTodo = function() {
 			var taskNameInput = $scope.todoInput.trim();
 			if (taskNameInput != '') {
-				$scope.todoList.push({taskName: taskNameInput, completed: false});
+				var taskId = $scope.createId()
+				$scope.todoList[taskId] = {taskId: taskId, taskName: taskNameInput, 
+					completed: false, editing: false};
 				$scope.todoInput = '';
 			};
 		};
 
 		$scope.checkState = function(){
 			var allSelected = true;
-			angular.forEach($scope.todoList, function(task){
+			angular.forEach($scope.todoList, function(task, taskId){
 				allSelected = allSelected && task.completed;
 			});
 			$scope.isAllCompleted = allSelected;
-
 		};
 
 		$scope.completeAll = function(){
-			angular.forEach($scope.todoList, function(task){
+			angular.forEach($scope.todoList, function(task, taskId){
 				task.completed = $scope.isAllCompleted;
 			});
 
@@ -34,7 +42,7 @@
 		$scope.clearCompleted = function() {
 			var oldList = $scope.todoList;
 			$scope.todoList = [];
-			angular.forEach(oldList, function(task) {
+			angular.forEach(oldList, function(task, taskId) {
 				if (!task.completed) {
 					$scope.todoList.push(task);
 				}
@@ -43,12 +51,17 @@
 
 		$scope.getNumCompleted = function() {
 			var counter = 0;
-			angular.forEach($scope.todoList, function(task) {
+			angular.forEach($scope.todoList, function(task, taskId) {
 				if (task.completed) {
 					counter++;
 				}
 			});
 			return counter;
+		};
+
+		$scope.toggleEditing = function(event) {
+			var clickedTaskId = event.target.parentNode.parentNode.dataset.taskid;
+			$scope.todoList[clickedTaskId].editing = true;
 		};
 	});
 
